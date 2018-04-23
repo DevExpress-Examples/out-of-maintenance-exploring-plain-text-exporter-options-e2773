1554aee3c8975981f81deb4215b11b0a181afb8b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/PlainTextExport/Form1.vb
@@ -0,0 +1,30 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Text
+Imports System.Windows.Forms
+Imports DevExpress.XtraRichEdit
+Imports System.IO
+Imports DevExpress.XtraRichEdit.Export
+
+Namespace PlainTextExport
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+            richEditControl1.LoadDocument("sample.docx", DocumentFormat.OpenXml)
+            richEditControl1.Options.Export.PlainText.ExportHiddenText = True
+        End Sub
+
+        Private Sub btnExport_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnExport.Click
+            Dim stream As New MemoryStream()
+            richEditControl1.SaveDocument(stream, DocumentFormat.PlainText)
+            stream.Position = 0
+            Dim sr As New StreamReader(stream, richEditControl1.Options.Export.PlainText.Encoding)
+            memoEdit1.Text = sr.ReadToEnd()
+        End Sub
+    End Class
+End Namespace
